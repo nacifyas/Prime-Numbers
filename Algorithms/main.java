67b238b4051cd1f2457bc2fc5bd1328b15b7832c
Yassine Nacif <nacifyas@gmail.com>
2021-03-11 10:32:13 +0100

    Basic Brute Force Algorithm

--- Algorithms/main.java
+++ Algorithms/main.java
@@ -3,8 +3,26 @@ package Algorithms;
 public class main {
 
     // Check Prime
+        // Basic Brute Force Algorithm:
+    public static boolean isPrimeI(Integer n) { /* O(sqrt(n)) */
+        boolean primality = true;
+        if (n % 2 == 0) { primality = false; }
+        else {
+            Integer r = floorRoot(n);
+            int i = 3;
+            while (i < r) {
+                if (n % i == 0) { primality = false; break;  }
+                i += 2; }
+        }
+        return primality;
+    }
 
     // Generate Primes
 
     // <PRIVATE> Update Primes Stack
+
+    // <PRIVATE> Auxiliary Methods
+    private static Integer floorRoot(Integer n) {
+        return (int) Math.floor(Math.sqrt(n));
+    }
 }
