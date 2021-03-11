75965adcd1a52d6f13709d14448e049060dd9995
Yassine Nacif <nacifyas@gmail.com>
2021-03-11 12:31:20 +0100

    Written Stack Update algorithm and some other overall imporvments

--- Algorithms/main.java
+++ Algorithms/main.java
@@ -9,7 +9,7 @@ public class main {
         // Basic Brute Force Algorithm:
     public static boolean isPrimeI(Integer n) { /* O(sqrt(n)) */
         boolean primality = true;
-        if (n % 2 == 0) { primality = false; }
+        if (n % 2 == 0 && n > 2) { primality = false; }
         else {
             Integer r = floorRoot(n);
             int i = 3;
@@ -23,12 +23,11 @@ public class main {
         // Stack Based Algorithm:
     public static boolean isPrimeII(Integer n) {
         boolean primality = false;
-
         if (primesArray.contains(n)) { primality = true; }
         else {
             Integer r = floorRoot(n);
-            int l = primesArray.size();
-            if (r <= primesArray.get(l)) {
+            int l = primesArray.size()-1;
+            if (r <= primesArray.get(l)) { /* This statement checks if there are enough primes in the stack to compute n's primality */
                 int i = 0;
                 while (i < l) {
                     int divisor = primesArray.get(l);
@@ -37,14 +36,18 @@ public class main {
             }
             else { System.out.println("Unable to compute primality: Not enough primes in the stack"); }
         }
-
         return primality;
     }
-        // <PRIVATE> Update Primes Stack
+        // <PRIVATE> Update Primes Stack:
     private static void updateStack(Integer n) {
-
+        int l = primesArray.size()-1;
+        Integer p = primesArray.get(l);
+        while (p <= n) {
+            if (isPrimeII(p)) { primesArray.add(p); }
+            p+=2;
+        }
     }
-
+        // The so called Stack of Primes:
     private static ArrayList<Integer> primesArray = new ArrayList<>();
 
 
